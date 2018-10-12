dbc74cac891eac8f9555bacaf21aec4796da431e
Rhuan Carvalho <rsjlcarvalho@gmail.com>
2018-10-12 13:33:13 -0300

    [UPD] Limpando projeto

--- CatalogoHQ/Startup.cs
+++ CatalogoHQ/Startup.cs
@@ -44,7 +44,7 @@ namespace CatalogoHQ
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Index/Error");
                 app.UseHsts();
             }
 
@@ -56,7 +56,7 @@ namespace CatalogoHQ
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Index}/{action=Index}/{id?}");
             });
         }
     }
