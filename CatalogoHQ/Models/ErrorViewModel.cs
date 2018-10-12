e05bc00dd3b0916aed031a20dd2f73f2d692875d
Rhuan Carvalho <rsjlcarvalho@gmail.com>
2018-10-11 23:33:31 -0300

    [ADD] Solucao CatalogoHQ

---
+++ CatalogoHQ/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CatalogoHQ.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
